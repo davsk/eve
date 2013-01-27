11749c42bcb440e7835061ccdb51994d8f79e3ee
David Skinner <skinner.david@gmail.com>
2013-01-26 19:15:35 -0600

    echo "Usage: ${0##*/} daily | monthly | static" added a few tables for evaluation

--- scripts/dl.sh
+++ scripts/dl.sh
@@ -2,6 +2,7 @@
 
 function load {
 	gzip -d tmp/*.gz
+	echo "Working!"
 	ls tmp/*.txt | awk '{print "./convert.awk "$1" > "$1".new; mv "$1".new "$1";psql eve -f "$1}' | sh
 
 	# debug
@@ -12,35 +13,49 @@ function load {
 
 mkdir -p tmp
 
-# THESE ARE UPDATED ONLY WHEN THE DATA CHANGES, YOU DON'T NEED THEM EVERY NIGHT.
-wget -P tmp http://eve-marketdata.com/developers/mysql.txt
+case "$1" in
+	"")
+		echo "Usage: ${0##*/} daily | monthly | static"
+		;;
+	"static")
+		# THESE ARE UPDATED ONLY WHEN THE DATA CHANGES, YOU DON'T NEED THEM EVERY NIGHT.
+		wget -P tmp http://eve-marketdata.com/developers/mysql.txt
 
-# fix a goof in the file
-sed -i 's/_system_id/_solarsystem_id/g' tmp/mysql.txt
+		# fix a goof in the file
+		sed -i 's/_system_id/_solarsystem_id/g' tmp/mysql.txt
 
-load
+		load
 
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_inv_marketgroups.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_regions.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_solarsystem_jumps.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_solarsystems.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_names.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_sta_stations.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_inv_marketgroups.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_regions.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_solarsystem_jumps.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_map_solarsystems.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_names.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_sta_stations.txt.gz
 
-load
+		load
+		;;
+	"daily")
+		# THESE ARE UPDATED ONCE A DAY AT DOWNTIME.
+		wget -P tmp http://eve-marketdata.com/developers/mysql_eve_inv_types.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_buying.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_selling.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_buying_jita.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_selling_jita.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_station_rank.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_region_type_updates.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_region_type_hist_updates.txt.gz
 
-# THESE ARE UPDATED ONCE A DAY AT DOWNTIME.
-wget -P tmp http://eve-marketdata.com/developers/mysql_eve_inv_types.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_items_buying.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_items_selling.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_station_rank.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_region_type_updates.txt.gz
-wget -P tmp http://eve-marketdata.com/developers/mysql_region_type_hist_updates.txt.gz
-
-load
-
-wget -P tmp http://eve-marketdata.com/developers/mysql_items_history.txt.gz
-gzip -d tmp/*.gz
-ls tmp/*.txt | awk '{print "./convert.awk "$1" > "$1".new; mv "$1".new "$1"; ./psql-split.sh "$1}'
+		load
+		;;
+	"monthly")
+		# updated daily but only needed monthly
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_history.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_history_30.txt.gz
+		wget -P tmp http://eve-marketdata.com/developers/mysql_items_history_theforge_90.txt.gz
+		gzip -d tmp/*.gz
+		ls tmp/*.txt | awk '{print "./convert.awk "$1" > "$1".new; mv "$1".new "$1"; ./psql-split.sh "$1}'
+		;;
+esac
 
 rmdir tmp
